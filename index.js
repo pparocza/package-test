01159fe7e385eac0ef112268bb39e965974e66ce
Paul Paroczai <paulparoczai@gmail.com>
2024-03-06 15:04:12 -0800

    comment out audio

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 exports.isWds = function(string)
 {
+    /*
     const AudioContext = window.AudioContext || window.webkitAudioContext;
     const audioContext = new AudioContext();
+    */
 
     // test 1
     console.log("Run Function!");
-    var oscillator = audioContext.createOscillator();
+    // var oscillator = audioContext.createOscillator();
     console.log("Oscillator created!");
     return string;
 }
