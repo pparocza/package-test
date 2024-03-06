99296be1a86e178dd678be9c86e9d964e596e13a
Paul Paroczai <paulparoczai@gmail.com>
2024-03-06 15:02:02 -0800

    attempt some audio

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 exports.isWds = function(string)
 {
+    const AudioContext = window.AudioContext || window.webkitAudioContext;
+    const audioContext = new AudioContext();
+
     // test 1
     console.log("Run Function!");
+    var oscillator = audioContext.createOscillator();
+    console.log("Oscillator created!");
     return string;
 }
