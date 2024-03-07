ea31327b5ebabc5bee63951d081c95547541f698
Paul Paroczai <paulparoczai@gmail.com>
2024-03-06 16:12:34 -0800

    restore audio

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 exports.isWds = function(string)
 {
-    // just for kicks
-    /*
     const AudioContext = window.AudioContext || window.webkitAudioContext;
     const audioContext = new AudioContext();
-    */
 
     // test 1
     console.log("Run Function!");
