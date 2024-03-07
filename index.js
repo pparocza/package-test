b36f4778ef20b8e7dc0201869dd0a7002a3c5afa
Paul Paroczai <paulparoczai@gmail.com>
2024-03-06 16:32:25 -0800

    re-scope audio context

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
+const AudioContext = window.AudioContext || window.webkitAudioContext;
+const audioContext = new AudioContext();
+
 exports.isWds = function(string)
 {
-    const AudioContext = window.AudioContext || window.webkitAudioContext;
-    const audioContext = new AudioContext();
-
     // test 1
     console.log("Run Function!");
     // var oscillator = audioContext.createOscillator();
